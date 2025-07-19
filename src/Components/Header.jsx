eccf222dfc1a36919a84526abe07bc48d56e2de0
Cinnamon <213962814+irunonironic@users.noreply.github.com>
2025-07-19 23:32:01 +0530

    add headings

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,20 +2,30 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className="flex flex-col items-center justify-center text-center mt-30 pt-14">
-
-      <h1 className="text-5xl italic text-gray-800 flex items-center gap-2">
-        Find the best
+    <div className="flex flex-col items-center justify-center text-center pt-24">
+      <h1
+        className="text-4xl lg:text-5xl font-medium leading-tight"
+        style={{
+          fontFamily: '"Playfair Display", Georgia, serif',
+        }}
+      >
+        <span>Find the best</span>{' '}
         <img
           src="/direct-flight_1565911.png"
           alt="flight"
-          className="w-8 h-8 inline"
-        />
-        flights faster
+          className="w-14 h-14 lg:w-12 lg:h-12 inline-block mx-1 align-middle"
+        />{' '}
+        <span>flights faster</span>
       </h1>
-      <h1 className="text-5xl italic text-gray-600 mt-4">
+      
+      <h2
+        className="text-4xl lg:text-5xl font-medium italic tracking-wide mt-4 text-slate-500"
+        style={{
+          fontFamily: '"Playfair Display", Georgia, serif',
+        }}
+      >
         Travel smarter
-      </h1>
+      </h2>
     </div>
   )
 }
